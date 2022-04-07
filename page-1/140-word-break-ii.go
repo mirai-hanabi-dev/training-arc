d300c1d23f334fa98dba594634b4a6777e8ac186
Minabi <mirai.hanabi.dev@gmail.com>
2022-04-08 00:35:59 +0700

    WIP

---
+++ page-1/140-word-break-ii.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func wordBreak(s string, wordDict []string) []string {
+	return []string{}
+}
+
+func main() {
+	s := "catsanddog"
+	wordDict := []string{"cat", "cats", "and", "sand", "dog"}
+	fmt.Println(wordBreak(s, wordDict))
+}
